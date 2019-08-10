872ae0c1c34776722acf792765a37ad154d5d8f9
ExactlyNoSense <whois.ard@gmail.com>
2019-08-11 02:34:44 +0300

    :white_check_mark: Add new test that checks dedent processing when there are many new lines in the end of a file

--- src/test/kotlin/idea/snakeskin/lang/lexer/SsLexerTestCase.kt
+++ src/test/kotlin/idea/snakeskin/lang/lexer/SsLexerTestCase.kt
@@ -5,4 +5,6 @@ class SsLexerTestCase : SsLexerTestCaseBase() {
 	fun testLineSplitting() = compare()
 
 	fun testBlock() = compare()
+
+	fun testHead() = compare()
 }
---
+++ src/test/resources/lexer/head.ss
@@ -0,0 +1,2 @@
+- head
+	- void x
