3acdd17c77cbe8cee109fd4a91c158fe27169cb6
ExactlyNoSense <whois.ard@gmail.com>
2019-08-31 02:38:45 +0300

    Use alias for DefaultLanguageHighlighterColors class

--- src/main/kotlin/idea/snakeskin/ide/color/SsColor.kt
+++ src/main/kotlin/idea/snakeskin/ide/color/SsColor.kt
@@ -1,13 +1,13 @@
 package idea.snakeskin.ide.color
 
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors as Default
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.options.colors.AttributesDescriptor
 
 enum class SsColor(uiName: String, val fallback: TextAttributesKey? = null) {
-	IDENTIFIER("Identifier", DefaultLanguageHighlighterColors.IDENTIFIER),
-	STRING("String", DefaultLanguageHighlighterColors.STRING),
-	NUMBER("Number", DefaultLanguageHighlighterColors.NUMBER),
+	IDENTIFIER("Identifier", Default.IDENTIFIER),
+	STRING("String", Default.STRING),
+	NUMBER("Number", Default.NUMBER),
 	;
 
 	val textAttributesKey = TextAttributesKey.createTextAttributesKey("idea.snakeskin.$name", fallback)
