2cd774150feb2c1c1aeedd3aac9dd5fc1a7c35a2
ExactlyNoSense <whois.ard@gmail.com>
2019-09-05 02:56:22 +0300

    Fix folding builder. Now it find blocks recursively and choose correct lines to fold

--- src/main/kotlin/idea/snakeskin/ide/folding/SsFoldingBuilder.kt
+++ src/main/kotlin/idea/snakeskin/ide/folding/SsFoldingBuilder.kt
@@ -6,8 +6,9 @@ import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiWhiteSpace
 import com.intellij.psi.util.PsiTreeUtil
-import idea.snakeskin.lang.psi.SsBlockBody
+import idea.snakeskin.lang.psi.*
 
 class SsFoldingBuilder : FoldingBuilderEx() {
 	override fun getPlaceholderText(node: ASTNode) = "{ ... }"
@@ -15,18 +16,46 @@ class SsFoldingBuilder : FoldingBuilderEx() {
 	override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
 		val descriptors = mutableListOf<FoldingDescriptor>()
 
-		fun findBlockBody(element: PsiElement) {
-			when(element){
-				is SsBlockBody -> {
+		fun findBlockBody(element: PsiElement): Int {
+			when (element) {
+				is SsBlockBody, is SsSwitchBlockBody, is SsLoopBlockBody, is SsCallBlockBody -> {
 					val rangeStart = PsiTreeUtil.skipWhitespacesBackward(element)?.textRange?.startOffset
 						?: element.textRange.startOffset
-					val rangeEnd = element.lastChild?.prevSibling?.textRange?.endOffset
-						?: element.textRange.endOffset
+
+					var rangeEnd = rangeStart
+					for (child in element.children) {
+						val end = findBlockBody(child)
+						rangeEnd = if (end > rangeEnd) end else rangeEnd
+					}
 					val textRange = TextRange(rangeStart, rangeEnd)
 					descriptors.add(FoldingDescriptor(element, textRange))
+					return rangeEnd
 				}
 
-				else -> element.children.forEach(::findBlockBody)
+				else -> {
+					if (element.children.isEmpty()) {
+						return element.textRange.endOffset
+					}
+					var last = element.lastChild
+					if (last is PsiWhiteSpace) {
+						last = PsiTreeUtil.skipWhitespacesBackward(last)
+					}
+					if (last == null) {
+						// element has only one child - white space. Don't take it in account
+						return element.textRange.startOffset
+					}
+					if (last is SsStatementEnd) {
+						return last.textRange.startOffset
+					}
+
+					var offsetEnd = element.textRange.startOffset
+					for (child in element.children) {
+						val end = findBlockBody(child)
+						offsetEnd = if (end > offsetEnd) end else offsetEnd
+					}
+
+					return offsetEnd
+				}
 			}
 		}
 
