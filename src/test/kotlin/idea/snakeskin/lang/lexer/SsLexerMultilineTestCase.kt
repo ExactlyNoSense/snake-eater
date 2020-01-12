0ffdb3c6e7ef3c3ac1a077b71c24a8c4cbded22f
ExactlyNoSense <whois.ard@gmail.com>
2020-01-12 20:59:31 +0300

    :white_check_mark: Add test for multiline feature: AMP token still can be recognized

--- src/test/kotlin/idea/snakeskin/lang/lexer/SsLexerMultilineTestCase.kt
+++ src/test/kotlin/idea/snakeskin/lang/lexer/SsLexerMultilineTestCase.kt
@@ -3,6 +3,7 @@ package idea.snakeskin.lang.lexer
 class SsLexerMultilineTestCase: SsLexerTestCaseBase() {
 	override fun getDirPath() = super.getDirPath() + "/multiline";
 
+	fun testAmpDoesNotTreatAsMlOpen() = compare()
 	fun testMlCloseOnNewLine() = compare()
 	fun testMlCloseOnNewLineWithSpace() = compare()
 	fun testMlCloseOnTheLastLine() = compare()
