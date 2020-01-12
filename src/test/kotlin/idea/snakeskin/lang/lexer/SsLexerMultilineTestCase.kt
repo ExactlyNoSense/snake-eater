ca108b0581192815d75421892d490c920b995ac3
ExactlyNoSense <whois.ard@gmail.com>
2020-01-12 19:41:19 +0300

    :white_check_mark: Add test for multiline feature

--- src/test/kotlin/idea/snakeskin/lang/lexer/SsLexerMultilineTestCase.kt
+++ src/test/kotlin/idea/snakeskin/lang/lexer/SsLexerMultilineTestCase.kt
@@ -6,4 +6,5 @@ class SsLexerMultilineTestCase: SsLexerTestCaseBase() {
 	fun testMlCloseOnNewLine() = compare()
 	fun testMlCloseOnNewLineWithSpace() = compare()
 	fun testMlCloseOnTheLastLine() = compare()
+	fun testMlOpenInTheMiddleOfExpression() = compare()
 }
