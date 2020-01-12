ee312ef069cafd13c3919f06a7fbd886cb8040ea
ExactlyNoSense <whois.ard@gmail.com>
2020-01-12 19:31:15 +0300

    :white_check_mark: Remove old line splitting tests

--- src/test/kotlin/idea/snakeskin/lang/lexer/SsLexerTestCase.kt
+++ src/test/kotlin/idea/snakeskin/lang/lexer/SsLexerTestCase.kt
@@ -2,8 +2,6 @@ package idea.snakeskin.lang.lexer
 
 class SsLexerTestCase : SsLexerTestCaseBase() {
 
-	fun testLineSplitting() = compare()
-
 	fun testBlock() = compare()
 
 	fun testHead() = compare()
